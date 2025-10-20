6d547987d5c6b93b67135a3ee1575569b99eb1f0
CECILIA ROMERO GRANADOS <cromero52207@ucq.edu.mx>
2025-10-20 16:31:59 -0600

    Refactor PrintArray function and comments

--- Templates.cpp
+++ Templates.cpp
@@ -3,27 +3,28 @@
 // Profesor: Adrián González
 // Universidad Cuauhtémoc Querétaro
 
-#include <iostream>          // biblioteca necesaria para usar cout y endl
-using namespace std;         // permite usar cout sin escribir std::cout
+#include <iostream> // Permite usar cout, endl, etc.
+using namespace std; // Simplifica el uso del espacio de nombres estándar
 
-// función plantilla (template) que permite imprimir arreglos de cualquier tipo de dato
-template <typename T>        // se usa <typename T> para hacer la función genérica
-void PrintArray(T* array, int size) // recibe un puntero al arreglo y su tamaño
+// Template de función que imprime los elementos de un arreglo de cualquier tipo
+template <typename T> // Declara que la función será genérica (puede usar cualquier tipo de dato)
+void PrintArray(T* array, int size)
 {
-    cout << "elementos del arreglo: "; // imprime un texto inicial antes de los valores
-    for (int i = 0; i < size; i++)     // ciclo que recorre todos los elementos del arreglo
-        cout << array[i] << " ";       // imprime el valor actual seguido de un espacio
-    cout << endl;                      // salto de línea al final
+    cout << "elementos del arreglo: "; // Muestra mensaje inicial
+    for (int i = 0; i < size; i++)     // Recorre todos los elementos del arreglo
+        cout << array[i] << " ";       // Imprime el elemento actual seguido de un espacio
+    cout << endl;                      // Salto de línea al final
 }
 
-// función que demuestra el uso del template PrintArray con distintos tipos de datos
+// Función que demuestra el uso del template PrintArray con diferentes tipos de datos
 void DemostracionTemplatePrintArray()
 {
-    int arr1[] = {1, 2, 3, 4, 5};                // arreglo de enteros
-    double arr2[] = {1.1, 2.2, 3.3};             // arreglo de números decimales (double)
-    string arr3[] = {"uno", "dos", "tres"};      // arreglo de cadenas de texto (string)
+    int arr1[] = {1, 2, 3, 4, 5};              // Arreglo de enteros
+    double arr2[] = {1.1, 2.2, 3.3};           // Arreglo de números decimales
+    string arr3[] = {"uno", "dos", "tres"};    // Arreglo de cadenas (strings)
 
-    PrintArray(arr1, 5);                         // imprime el arreglo de enteros
-    PrintArray(arr2, 3);                         // imprime el arreglo de decimales
-    PrintArray(arr3, 3);                         // imprime el arreglo de strings
+    // Llamadas a la función template con diferentes tipos
+    PrintArray(arr1, 5); // Imprime el arreglo de enteros
+    PrintArray(arr2, 3); // Imprime el arreglo de flotantes
+    PrintArray(arr3, 3); // Imprime el arreglo de cadenas
 }
